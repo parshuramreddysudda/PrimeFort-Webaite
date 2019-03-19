afcb7ddcda90512e657ddf2dfc86510805e246a3
parshuramreddysudda <parshuram.sudda@gmail.com>
2019-03-19 20:39:16 +0530

    Files Added

---
+++ js/Navi.js
@@ -0,0 +1,53 @@
+    
+ function serviceHoveredFunc()
+ {
+	document.getElementById("serviceHovered").style.transition = "all 2s";
+	 document.getElementById('serviceHovered').style.display='block';
+
+//	 document.getElementById('home').style.marginTop='-13%';
+
+	
+ }
+
+   function serviceRemovedFunc()
+ { 
+	document.getElementById("serviceHovered").style.transition = "all 2s";
+	 document.getElementById('serviceHovered').style.display='none';
+	 document.getElementById('home').style.marginTop='0%';
+	
+ }
+             
+        
+//    
+// function serviceHoveredFunc()
+// {
+////	document.getElementById("serviceHovered").style.transition = "all 2s";
+//	 document.getElementById('serviceHovered').style.visibility='visible';
+// document.getElementById('serviceHovered').style.display='block';
+//document.getElementById('serviceHovered').style.opacity='1';
+//     document.getElementById('serviceHovered').style.transitionDelay='0s';
+//	 document.getElementById('home').style.marginTop='-13%';
+//
+//	
+// }
+//
+//   function serviceRemovedFunc()
+// { 
+////	document.getElementById("serviceHovered").style.transition = "all 2s";
+//	 document.getElementById('serviceHovered').style.display='none';
+//    
+//       document.getElementById('serviceHovered').style.visibility='hidden';
+//      document.getElementById('serviceHovered').style.opacity='0';
+//      document.getElementById('serviceHovered').style.transition='visibility 0s linear 0.33s, opacity 0.33s linear';
+//     
+//      
+//	 document.getElementById('home').style.marginTop='0%';
+//	
+// }
+//             
+//        
+
+
+
+
+ 
